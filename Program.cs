b6f54b7d47922aeb27e1fcda6b712bad4402cb57
Andrew Edmonds <edmondsandrew9@gmail.com>
2022-07-27 23:07:24 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what is your favorite subject?");
+            int favSubject = int.Parse(Console.ReadLine());
+            switch (favSubject)
+            {
+                case 0:
+                    Console.WriteLine("Math");
+                    break;
+                case 1:
+                    Console.WriteLine("English");
+                    break;
+                case 2:
+                    Console.WriteLine("Science");
+                    break;
+                case 3:
+                    Console.WriteLine("History");
+                    break;
+                case 5:
+                    Console.WriteLine("Gym");
+                    break;
+            }
+
+        }
+    }
+}
